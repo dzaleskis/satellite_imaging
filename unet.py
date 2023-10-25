4ab7cbd48643cd750fc11a72e1894e6dc24587a5
Deividas Zaleskis <rr612788+DFIN@fincoad.com>
2023-10-25 12:09:22 +0300

    refactor

--- unet.py
+++ unet.py
@@ -13,12 +13,13 @@ from keras.layers import Input, concatenate, Convolution2D, MaxPooling2D, UpSamp
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as K
-K.set_image_dim_ordering('th')
 from sklearn.metrics import jaccard_score
 from shapely.geometry import MultiPolygon, Polygon
 import shapely.wkt
 import shapely.affinity
 from collections import defaultdict
+
+K.set_image_dim_ordering('th')
 
 N_Cls = 10
 inDir = '.'
