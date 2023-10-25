778d5546403d4410d922ea68683d57547ae1bd3f
Deividas Zaleskis <rr612788+DFIN@fincoad.com>
2023-10-25 12:08:50 +0300

    fix image dim ordering

--- unet.py
+++ unet.py
@@ -13,6 +13,7 @@ from keras.layers import Input, concatenate, Convolution2D, MaxPooling2D, UpSamp
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as K
+K.set_image_dim_ordering('th')
 from sklearn.metrics import jaccard_score
 from shapely.geometry import MultiPolygon, Polygon
 import shapely.wkt
